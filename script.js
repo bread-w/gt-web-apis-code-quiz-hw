f0157e914dee08770b7e7029579c9ee6e78b5324
Brad Williams <jb.wms89@gmail.com>
2020-06-10 21:17:47 -0400

    finished timer and now trying to work on the display properties for the two container blocks. I want the first container to hide and the second container to apper when the start quiz buitton is clicked

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-console.log("Hello World!")
+console.log("Hello World!");
 
 var startQuizEl = document.getElementById("startquiz");
 var timerEl = document.getElementById("timer");
-
+var frontPageEl = document.getElementById("front-page");
+var quizPageEl = document.getElementById("quiz-page");
 
 var currentQuestionIndex = 0;
 
-var arrayOfQuestions = [];
+var arrayOfQuestions = [
+  "Commonly used types of date DO NOT include:",
+  "The condition in an if/else statement is enclosed within _____.",
+  "Arrays in Javascript can be used to store _____.",
+  "String values must be enclosed within _____ when being assigned to variables.",
+  "A very useful tool used during development and debugging for printing content to the debugger is:",
+];
 
-var currentAnswerIndex = 0;
+var currentFirstAnswerIndex = 0;
 
-var arrayofAnswers = [];
+var arrayofFirstAnswers = [];
 
 secondsLeft = 75;
 
 function startTimer() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timerEl.textContent = "Time: " + secondsLeft;
-    }, 1000);
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timerEl.textContent = "Time: " + secondsLeft;
+  }, 1000);
+}
+
+function quizStart() {
+    if (frontPageEl.style.display === "block") {
+        frontPageEl.style.display = "none";
+    } if (quizPageEl.style.display === "none") {
+        quizPageEl.style.display = "block";
+    }
 }
 
 startQuizEl.addEventListener("click", startTimer);
+startQuizEl.addEventListener("click", quizStart);
